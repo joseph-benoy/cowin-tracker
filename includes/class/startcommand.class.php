221f36e2536cc2283015a6c4941d8a735c8691c4
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-23 01:58:07 +0530

    chenged

--- includes/class/startcommand.class.php
+++ includes/class/startcommand.class.php
@@ -9,7 +9,7 @@
         public function handle(){
             $this->replyWithMessage(['text'=>'<b>bold</b>, <strong>bold</strong>
             <i>italic</i>, <em>italic</em>
-            <a href="URL">inline URL</a>
+            <a href="https://www.google.com">inline URL</a>
             <code>inline fixed-width code</code>
             <pre>pre-formatted fixed-width code block</pre>',
             'parse_mode'=>'HTML']);
