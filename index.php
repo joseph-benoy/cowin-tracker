058001dd0af5e4b333b9c2bfec190fb5e871a31b
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-22 23:27:25 +0530

    changes to index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     require_once("vendor/autoload.php");
     use Telegram\Bot\Api;
-    $bot = new Api('1755386616:AAFH3PIzoumgJn1nOEy-i_YV8evDUUWq0qk',true);
+    $bot = new Api('1755386616:AAFH3PIzoumgJn1nOEy-i_YV8evDUUWq0qk');
     $me= $bot->getMe();
-    print_r($me->getId());
+    print_r($me);
 ?>
