0ad59dee39a4c989c01e765e2d6ae8beec0539bd
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-23 00:53:40 +0530

    added startcommand class

---
+++ includes/class/startcommand.class.php
@@ -0,0 +1,21 @@
+<?php
+    namespace Vendor\App\Commands;
+
+    use Telegram\Bot\Actions;
+    use Telegram\Bot\Commands\Command;
+    class startCommand extends Command{
+        protected $name = "start";
+        protected $description = "Start command to get started";
+        public function handle($args){
+            $this->replyWithMessage(['text'=>'Hello! welcome to Cowin vaccine tracker. Here are our available commands : ']);
+            $this->replyWithChatAction(['action'=>Actions::TYPING]);
+            $commands = $this->getTelegram()->getCommands();
+            $response = "";
+            foreach($commands as $name=>$command){
+                $response.=sprintf("/%s - /%s".PHP_EOL,$name,$command->getDescription());
+            }
+            $this->replyWithMessage(['text'=>$response]);
+            $this->triggerCommand('subscribe');
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -3,7 +3,10 @@
     require_once("includes/config.php");
     use Telegram\Bot\Api;
     use Api\Cowin;
+    use Vendor\App\Commands\startCommand;
     $bot = new Api('1755386616:AAFH3PIzoumgJn1nOEy-i_YV8evDUUWq0qk');
     $cowin = new Cowin();
-    print_r($cowin->get_states());
+    $startCmd = new startCommand();
+    $bot->addCommand($startCmd);
+    $update = $bot->commandsHandler(true);
 ?>
