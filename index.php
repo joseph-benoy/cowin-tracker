0b1168a85c4f48a2d8baee9c808a7caf86970155
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-22 23:25:23 +0530

    changed index file

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once("telegram_api/vendor/autoload.php");
+    require_once("vendor/autoload.php");
     use Telegram\Bot\Api;
     $bot = new Api('1755386616:AAFH3PIzoumgJn1nOEy-i_YV8evDUUWq0qk',true);
     $me= $bot->getMe();
