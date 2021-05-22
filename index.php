be407c40a55de8e1eb66dc957f97976a6dfbe93b
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-22 23:05:34 +0530

    added telegram sdk

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    require_once("telegram_api/vendor/autoload.php");
+    use Telegram\Bot\Api;
+    $bot = new Api('1755386616:AAFH3PIzoumgJn1nOEy-i_YV8evDUUWq0qk',true);
+    $me= $bot->getMe();
+    
+?>
