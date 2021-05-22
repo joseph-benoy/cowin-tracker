3d8dff55cb29535b63648cf9c03277263e8c17cd
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-22 23:38:59 +0530

    changed config file

--- includes/config.php
+++ includes/config.php
@@ -2,6 +2,6 @@
     spl_autoload_register(function($class){
         $pos= strripos($class,'\\')+1;
         $class_name = strtolower(substr($class,$pos));
-        require_once (__DIR__."/includes/class/{$class_name}.class.php");
+        require_once (__DIR__."/class/{$class_name}.class.php");
     });
 ?>
--- index.php
+++ index.php
@@ -2,5 +2,8 @@
     require_once("vendor/autoload.php");
     require_once("includes/config.php");
     use Telegram\Bot\Api;
+    use Api\Cowin;
     $bot = new Api('1755386616:AAFH3PIzoumgJn1nOEy-i_YV8evDUUWq0qk');
+    $cowin = new Cowin();
+    print_r($cowin->get_states());
 ?>
