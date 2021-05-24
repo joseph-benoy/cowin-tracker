28ae8ac868e0e367b36f830d9bca7d3d361cfd9c
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-24 16:14:04 +0530

    ff

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 try{
-    apc_add("x","value_XXXX",10000);
-    echo apc_fetch("x");
+    apcu_add("x","value_XXXX",10000);
+    echo apcu_fetch("x");
+}
 catch(Exception $e){
     echo $e->getMessage();
 }
