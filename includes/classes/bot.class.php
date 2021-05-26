e9760cc3ca159ee08bcace49ac4c2c94bbd63997
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-26 23:47:35 +0530

    refactering bot class

--- includes/classes/bot.class.php
+++ includes/classes/bot.class.php
@@ -1,17 +1,47 @@
 <?php
     namespace Telegram\Api;
-    class Bot extends Action{
-        protected $classArray=[];
-        protected $callbackQueryArray=[];
-        public function __construct($token,$classArray=null){
+    class Bot{
+        public $apiToken;
+        public $updateObj;
+        public function __construct($token){
             $this->apiToken = $token;
-            if($classArray!=null){
-                foreach($classArray as $class){
-                    if(!in_array($class,$this->classArray)){
-                        array_push($this->classArray,$class);
-                    }
-                }
+        }
+        public function sendReply($method,$data){
+            $url = "https://api.telegram.org/bot".$this->apiToken. "/" . $method;
+            $curl = curl_init();
+            if($method!='sendMessage'){
+                curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                    "Content-Type:multipart/form-data"
+                ));
             }
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_URL, $url); 
+            $output = curl_exec($curl);
+            curl_close($curl);
+            return $output;
+        }
+        public function getChatId():string{
+            if(property_exists($this->updateObj,"message")){
+                return $this->updateObj->message->chat->id;
+            }
+            else{
+                return $this->updateObj->callback_query->from->id;
+            }
+        }
+        public function replyChatAction($action="typing"){
+            $data = array("chat_id"=>$this->getChatId(),"action"=>$action);
+            $this->sendReply("sendChatAction",$data);
+        }
+        public function getText(){
+            return $this->updateObj->message->text;
+        }
+        public function getUsername(){
+            return $this->updateObj->from->username;
+        }
+        public function getFullname(){
+            return  $this->updateObj->from->first_name." ".$this->updateObj->from->last_name;
         }
         protected function isCommand($text){
             if(substr($text,0,1)=="/"){
@@ -21,86 +51,14 @@
                 return false;
             }
         }
-        protected function filterUpdate($updateObj):\stdClass{
-            if(property_exists($updateObj,'callback_query')){
-                    $filterObj = new \stdClass;
-                    $filterObj->chatId = $updateObj->callback_query->from->id;
-                    $filterObj->queryData =$updateObj->callback_query->data;
-                    return $filterObj;
-            }
-            elseif($this->isCommand($updateObj->message->text)){
-                $cmd = $this->getCommandName($updateObj->message->text);
-                if($cmd!=null){
-                    $filterObj = new \stdClass;
-                    $filterObj->chatId = $updateObj->message->chat->id;
-                    $filterObj->commandName = $cmd;
-                    return $filterObj;
-                }
-                else{
-                    $filterObj = new \stdClass;
-                    $filterObj->chatId = $updateObj->message->chat->id;
-                    $filterObj->commandName = "";
-                    return $filterObj;
-                }
-            }
-            else{
-                $filterObj = new \StdClass;
-                $filterObj->chatId = $updateObj->message->chat->id;
-                $filterObj->text = $updateObj->message->text;
-                return $filterObj;
-            }
-        }
-        protected function getCommandName($text){
-            $data = ltrim($text,"/");
-            if(in_array($data,$this->classArray)){
-                return $data;
-            }
-            else{
-                return null;
-            }
-        }
         public function capture():void{
-            $updateObj = json_decode(file_get_contents("php://input"));
-            $filterObj = $this->filterUpdate($updateObj);
-            if(property_exists($filterObj,'queryData')){
-                //execute as callback_query
-                if($filterObj->queryData==""){
-                    //$this->sendError($filterObj,"Invalid callback query");
+            $this->updateObj = json_decode(file_get_contents("php://input"));
+            if($this->isCommand($this->getText())){
+                if($this->getText()==="/start"){
+                    $this->startCommand();
                 }
                 else{
-                    $this->executeCallbackQuery($filterObj,$updateObj);
-                }
-            }
-            elseif(property_exists($filterObj,'commandName')){
-                //execute as registered command
-                if($filterObj->commandName==""){
-//                    $this->sendError($filterObj,"Invalid Command");// send error message because command is not registered
-                }
-                else{
-                    $this->executeCommand($filterObj,$updateObj);
-                }
-            }
-            else{
-                //execute as random input
-                $this->routeRandomInput($filterObj,$updateObj);
-            }
-        }
-        protected function executeCommand($filterObj,$updateObj):void{
-            if(!apcu_exists($filterObj->chatId)){
-                $class = $filterObj->commandName;
-                $command = new $class($this->apiToken,$updateObj);
-                $command->handle();
-            }
-            else{
-                $sessionId = apcu_fetch($filterObj->chatId);
-                error_log("SSSSSSSSSSSSSSSSSs = {$sessionId}",0);
-//                $this->sendError($filterObj,"Invalid input");
-            }
-        }
-        public function registerCommands($class_array){
-            foreach($class_array as $class){
-                if(!in_array($class,$this->classArray)){
-                    array_push($this->classArray,$class);
+                    error_log("##Not start command",0);
                 }
             }
         }
@@ -111,24 +69,38 @@
                 return "";
             }
         }
-        protected function executeCallbackQuery($filterObj,$updateObj):void{
-            $commandSessionObj = json_decode($this->getCommandSession($filterObj->chatId));
-            if($commandSessionObj!=""){
-                $class = $commandSessionObj->commandName;
-                $command = new $class($this->apiToken,$updateObj);
-                $command->handle(null,$commandSessionObj,$filterObj->queryData);
+        public function replyMessage($text,$replyMarkup=null){
+            if($replyMarkup!=null){
+                $data = array("chat_id"=>$this->getChatId(),"text"=>$text,"reply_markup"=>$replyMarkup);
+                $this->sendReply('sendMessage',$data);
+            }
+            else{
+                $data = array("chat_id"=>$this->getChatId(),"text"=>$text);
+                $this->sendReply('sendMessage',$data);
             }
         }
-        protected function routeRandomInput($filterObj,$updateObj){
-            $commandSessionObj = json_decode($this->getCommandSession($filterObj->chatId));
-            $sessionId = $commandSessionObj->sessionName;
-            error_log("SSSSSSSSSSSSSSSSSs = {$sessionId}",0);
-            if($commandSessionObj!=""){
-                $class = $commandSessionObj->commandName;
-                $command = new $class($this->apiToken,$updateObj);
-                $command->handle($filterObj->text,$commandSessionObj,null);
+        public function setCommandSession($sessionName){
+            $obj = new \stdClass;
+            $obj->commandName = get_class($this);
+            $obj->sessionName = $sessionName;
+            if(!apcu_exists($this->getChatId())){
+                apcu_add($this->getChatId(),json_encode($obj));
             }
+            else{
+                apcu_store($this->getChatId(),json_encode($obj));
+            }
+        }
+        public function deleteCommandSession(){
+            apcu_delete($this->getChatId());
+        }
+        public function startCommand(){
+            $keyboardMarkup = new Telegram\component\ReplyKeyboard();
+            $keyboardMarkup->addRow([["text"=>"List sessions by District","callback_data"=>"List sessions by District"]]);
+            $keyboardMarkup->addRow([["text"=>"List sessions by Pincode","callback_data"=>"List sessions by Pincode"]]);
+            $keyboardMarkup->addRow([["text"=>"Add District to watchlist","callback_data"=>"Add district to watchlist"]]);
+            $keyboardMarkup->addRow([["text"=>"Add Pincode to watchlist","callback_data"=>"Add Pincode to watchlist"]]);
+            $result = $this->replyMessage("*Hello {$this->getFullname()}!*Welcome to Cowin Tracker! We will help you updated with the availability of vaccine sessions within your district or your pincode area.\n\n*Choose appropriate option from the menu*\n_Please don't spam with random inputs_","markdown",$keyboardMarkup->getMarkup());
+            error_log("##start command executed{$result}",0);
         }
     }
-
 ?> 
