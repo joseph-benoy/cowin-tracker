b70bb92e8ba8b830ccc5840519aad29041ab542a
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-27 21:19:02 +0530

    removed git ignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '426b9f93acdbd2d359c23895d20681c6f1557df0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '426b9f93acdbd2d359c23895d20681c6f1557df0',
+    ),
+  ),
+);
